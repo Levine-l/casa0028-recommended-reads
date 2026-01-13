c9a143812bcf96de1fa6ed4ddcb014650dd8786c
ddunc23 <d.hay@ucl.ac.uk>
2026-01-13 11:05:25 +0000

    added initial data and TitleBar component

--- src/components/TitleBar.jsx
+++ src/components/TitleBar.jsx
@@ -3,7 +3,7 @@ function TitleBar() {
         <header className="bg-white">
             <div className="mx-auto h-16 pt-4 gap-8 px-4 sm:px-6 lg:px-8">
                 <h3 className="text-center text-lg font-bold italic">
-                    📚 OpenPlaques Reading Recommendations
+                    📚 Recommended Reading
                 </h3>
             </div>
         </header>
