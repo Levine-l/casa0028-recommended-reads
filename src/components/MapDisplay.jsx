9fffb543dca066ec610bf4b5234c9c71bf5450b3
Levine-l <ucfnhl0@ucl.ac.uk>
2026-01-22 16:04:05 +0000

    modal and map.

--- src/components/MapDisplay.jsx
+++ src/components/MapDisplay.jsx
@@ -4,15 +4,17 @@ import 'maplibre-gl/dist/maplibre-gl.css';
 
 export default function MapDisplay(props) {
   return (
-    <Map
-      initialViewState={{
-        longitude: props.longitude,
-        latitude: props.latitude,
-        zoom: props.zoom
-      }}
-      style={{width: 600, height: 400}}
-      mapStyle="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
-    />
+    <div style={{width: '100%', height: 600}}>
+      <Map
+        initialViewState={{
+          longitude: props.longitude,
+          latitude: props.latitude,
+          zoom: props.zoom
+        }}
+        style={{width: '100%', height: '100%'}}
+        mapStyle="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
+      />
+    </div>
   );
 }
 
--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,6 @@ export default defineConfig({
     }),
     tailwindcss(),
   ],
-  base: '/casa0028-recommended-reads/'
-  homepage:'https://Levine-l.github.io/casa0028-recommended-reads/'
+  base: '/casa0028-recommended-reads/',
+  homepage: 'https://Levine-l.github.io/casa0028-recommended-reads/'
 })
